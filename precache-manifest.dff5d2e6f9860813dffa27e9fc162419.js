d7dfa191906c3a80d2fa94e048bd03aacc16e12d
fStarck <felipegstarck@gmail.com>
2024-12-26 21:10:47 -0300

    Updates

---
+++ precache-manifest.dff5d2e6f9860813dffa27e9fc162419.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73ce519874cf88b2158095c2c9e0740d",
+    "url": "/LpCorpusAcademias2/index.html"
+  },
+  {
+    "revision": "24e2c5c84cd6fecaa4be",
+    "url": "/LpCorpusAcademias2/static/css/2.5a2a4169.chunk.css"
+  },
+  {
+    "revision": "e437b1edc383b518fa95",
+    "url": "/LpCorpusAcademias2/static/css/main.4754caef.chunk.css"
+  },
+  {
+    "revision": "24e2c5c84cd6fecaa4be",
+    "url": "/LpCorpusAcademias2/static/js/2.5f08ba36.chunk.js"
+  },
+  {
+    "revision": "e437b1edc383b518fa95",
+    "url": "/LpCorpusAcademias2/static/js/main.3fe783f3.chunk.js"
+  },
+  {
+    "revision": "8c711d57afb9406b8b49",
+    "url": "/LpCorpusAcademias2/static/js/runtime~main.f2ebce58.js"
+  },
+  {
+    "revision": "5824d0835ab2c56e1ee364e15d84c784",
+    "url": "/LpCorpusAcademias2/static/media/Bozon-Regular.5824d083.ttf"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/LpCorpusAcademias2/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/LpCorpusAcademias2/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/LpCorpusAcademias2/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/LpCorpusAcademias2/static/media/slick.f97e3bbf.svg"
+  }
+]);
